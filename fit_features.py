d8ebb1ec1f7d3afdebd4a0c19c3346373b03b221
mdecleir <mdecleir@stsci.edu>
2024-05-10 15:51:31 -0400

    Added functions for the 3.4 and 6.2 micron features

--- fit_features.py
+++ fit_features.py
@@ -85,6 +85,142 @@ def fit_feature(outpath, star, model, waves, taus, uncs, feat_name):
     return fit_result_feat_emcee, chains
 
 
+def fit_34(datapath, star):
+    """
+    Function to fit the continuum and the feature at 3.4 micron
+
+    Parameters
+    ----------
+    datapath : string
+        Path to the data files
+
+    star : string
+        Star name
+
+    Returns
+    -------
+    fit_result_feat_emcee : astropy model
+        MCMC fitting results
+
+    chains : numpy.ndarray
+        MCMC chains (for amplitude, mean and stddev in that order)
+
+    Saves plots with data and fitted models
+    """
+    # obtain the data
+    starname = "hd" + star.split("D")[1].strip("0")
+    data = Table.read(datapath + "MIRI/v3/" + starname + "_nircam_mrs_merged.fits")
+    waves = data["wavelength"]
+    fluxes = data["flux"]
+
+    # define masks for the continuum fitting
+    rangemask = (waves > 3.25) & (waves <= 3.55)
+    feat_reg_mask = (waves > 3.32) & (waves <= 3.5)
+    stellar_lines_out = ((waves > 3.28) & (waves <= 3.32)) | (
+        (waves > 5.947) & (waves <= 5.969)
+    )
+    # stellar line masks per star:
+    # 5.89-5.913, 5.903-5.913, 5.887-5.922, 5.894-5.918, 5.898-5.926, 5.894-5.911
+    # 5.948-5.957, 5.951-5.969, 5.947-5.964
+    cont_mask = rangemask & ~feat_reg_mask & ~stellar_lines_out & ~np.isnan(fluxes)
+
+    # fit the continuum
+    pol_mod = models.Polynomial1D(3)
+    lin_fitter = fitting.LinearLSQFitter()
+    fit_result_cont = lin_fitter(
+        pol_mod,
+        waves[cont_mask],
+        fluxes[cont_mask] * waves[cont_mask] ** 2,
+    )
+
+    # plot the data and the continuum fit
+    fig, axes = plt.subplots(
+        2,
+        sharex=True,
+        figsize=(9, 10),
+        gridspec_kw={"hspace": 0},
+    )
+    fs = 18
+    axes[0].plot(
+        waves[rangemask], fluxes[rangemask] * waves[rangemask] ** 2, c="k", alpha=0.9
+    )
+    axes[0].plot(
+        waves[rangemask],
+        fit_result_cont(waves[rangemask]),
+        c="tab:orange",
+        label="cont. fit",
+    )
+
+    # plot the data points that were used in the continuum fitting
+    axes[0].plot(
+        waves[cont_mask],
+        fluxes[cont_mask] * waves[cont_mask] ** 2,
+        "r.",
+        markersize=8,
+        alpha=0.8,
+        label="fit points",
+    )
+    axes[0].set_ylabel("flux (Jy)", fontsize=fs)
+
+    # normalize the fluxes
+    norm_fluxes = (
+        fluxes[rangemask] * waves[rangemask] ** 2 / fit_result_cont(waves[rangemask])
+    )
+
+    # convert to optical depth (only exists in rangemask)
+    taus = np.log(1 / norm_fluxes)
+
+    # calculate the empirical uncertainy on the optical depth in clean regions
+    unc = np.std(taus[cont_mask[rangemask]])
+
+    # mask the stellar lines inside the feature and define the mask for the feature fitting
+    stellar_lines_in = ((waves > 5.703) & (waves <= 5.717)) | (
+        (waves > 5.823) & (waves <= 5.841)
+    )
+    # stellar line masks per star:
+    # 5.705-5.717, 5.706-5.715, 5.703-716, 5.712-5.717, 5.711-5.717
+    # 5.823-5.831, 5.824-5.836, 5.827-5.839, 5.829-5.841, 5.825-5.836, 5.823-5.840
+    feat_full_mask = feat_reg_mask & ~stellar_lines_in
+    feat_fit_mask = feat_full_mask[rangemask] & ~np.isnan(taus)
+
+    # define the uncertainties
+    uncs = np.full(len(taus[feat_fit_mask]), unc)
+
+    # define the initial model to fit the feature
+    gauss_mod = models.Gaussian1D(
+        amplitude=0.01,
+        stddev=0.02,
+        mean=3.4,
+        bounds={
+            "mean": (3.38, 3.44),
+            "stddev": (0.05, 0.11),
+        },
+    )
+
+    # fit the feature
+    fit_result_feat_emcee, chains = fit_feature(
+        datapath + "MIRI/",
+        star,
+        gauss_mod,
+        waves[rangemask][feat_fit_mask],
+        taus[feat_fit_mask],
+        uncs,
+        "34",
+    )
+
+    # plot the feature fits
+    axes[1].plot(waves[rangemask], taus, c="k", alpha=0.9)
+    axes[1].plot(
+        waves[rangemask], fit_result_feat_emcee(waves[rangemask]), c="crimson", lw=2
+    )
+    axes[1].set_xlabel(r"wavelength ($\mu$m)", fontsize=fs)
+    axes[1].set_ylabel("optical depth", fontsize=fs)
+    axes[1].axhline(ls=":", c="k")
+    plt.savefig(datapath + star + "_34_fit.pdf", bbox_inches="tight")
+
+    return fit_result_feat_emcee, chains
+
+
 def fit_58(datapath, star):
     """
     Function to fit the continuum and the feature at 5.8 micron
@@ -216,6 +352,142 @@ def fit_58(datapath, star):
     axes[1].set_ylabel("optical depth", fontsize=fs)
     axes[1].axhline(ls=":", c="k")
     plt.savefig(datapath + star + "_58_fit.pdf", bbox_inches="tight")
+
+    return fit_result_feat_emcee, chains
+
+
+def fit_62(datapath, star):
+    """
+    Function to fit the continuum and the feature at 6.2 micron
+
+    Parameters
+    ----------
+    datapath : string
+        Path to the data files
+
+    star : string
+        Star name
+
+    Returns
+    -------
+    fit_result_feat_emcee : astropy model
+        MCMC fitting results
+
+    chains : numpy.ndarray
+        MCMC chains (for amplitude, mean and stddev in that order)
+
+    Saves plots with data and fitted models
+    """
+    # obtain the data
+    starname = "hd" + star.split("D")[1].strip("0")
+    data = Table.read(datapath + "MIRI/v3/" + starname + "_nircam_mrs_merged.fits")
+    waves = data["wavelength"]
+    fluxes = data["flux"]
+
+    # define masks for the continuum fitting
+    rangemask = (waves > 5.9) & (waves <= 6.6)
+    feat_reg_mask = (waves > 6.1) & (waves <= 6.44)
+    stellar_lines_out = ((waves > 5.887) & (waves <= 5.926)) | (
+        (waves > 5.947) & (waves <= 5.969)
+    )
+    # stellar line masks per star:
+    # 5.89-5.913, 5.903-5.913, 5.887-5.922, 5.894-5.918, 5.898-5.926, 5.894-5.911
+    # 5.948-5.957, 5.951-5.969, 5.947-5.964
+    cont_mask = rangemask & ~feat_reg_mask & ~stellar_lines_out & ~np.isnan(fluxes)
+
+    # fit the continuum
+    pol_mod = models.Polynomial1D(3)
+    lin_fitter = fitting.LinearLSQFitter()
+    fit_result_cont = lin_fitter(
+        pol_mod,
+        waves[cont_mask],
+        fluxes[cont_mask] * waves[cont_mask] ** 2,
+    )
+
+    # plot the data and the continuum fit
+    fig, axes = plt.subplots(
+        2,
+        sharex=True,
+        figsize=(9, 10),
+        gridspec_kw={"hspace": 0},
+    )
+    fs = 18
+    axes[0].plot(
+        waves[rangemask], fluxes[rangemask] * waves[rangemask] ** 2, c="k", alpha=0.9
+    )
+    axes[0].plot(
+        waves[rangemask],
+        fit_result_cont(waves[rangemask]),
+        c="tab:orange",
+        label="cont. fit",
+    )
+
+    # plot the data points that were used in the continuum fitting
+    axes[0].plot(
+        waves[cont_mask],
+        fluxes[cont_mask] * waves[cont_mask] ** 2,
+        "r.",
+        markersize=8,
+        alpha=0.8,
+        label="fit points",
+    )
+    axes[0].set_ylabel("flux (Jy)", fontsize=fs)
+
+    # normalize the fluxes
+    norm_fluxes = (
+        fluxes[rangemask] * waves[rangemask] ** 2 / fit_result_cont(waves[rangemask])
+    )
+
+    # convert to optical depth (only exists in rangemask)
+    taus = np.log(1 / norm_fluxes)
+
+    # calculate the empirical uncertainy on the optical depth in clean regions
+    unc = np.std(taus[cont_mask[rangemask]])
+
+    # mask the stellar lines inside the feature and define the mask for the feature fitting
+    stellar_lines_in = ((waves > 5.703) & (waves <= 5.717)) | (
+        (waves > 5.823) & (waves <= 5.841)
+    )
+    # stellar line masks per star:
+    # 5.705-5.717, 5.706-5.715, 5.703-716, 5.712-5.717, 5.711-5.717
+    # 5.823-5.831, 5.824-5.836, 5.827-5.839, 5.829-5.841, 5.825-5.836, 5.823-5.840
+    feat_full_mask = feat_reg_mask & ~stellar_lines_in
+    feat_fit_mask = feat_full_mask[rangemask] & ~np.isnan(taus)
+
+    # define the uncertainties
+    uncs = np.full(len(taus[feat_fit_mask]), unc)
+
+    # define the initial model to fit the feature
+    gauss_mod = models.Gaussian1D(
+        amplitude=0.01,
+        stddev=0.02,
+        mean=6.2,
+        bounds={
+            "mean": (6.1, 6.3),
+            "stddev": (1e-4, 0.15),
+        },
+    )
+
+    # fit the feature
+    fit_result_feat_emcee, chains = fit_feature(
+        datapath + "MIRI/",
+        star,
+        gauss_mod,
+        waves[rangemask][feat_fit_mask],
+        taus[feat_fit_mask],
+        uncs,
+        "62",
+    )
+
+    # plot the feature fits
+    axes[1].plot(waves[rangemask], taus, c="k", alpha=0.9)
+    axes[1].plot(
+        waves[rangemask], fit_result_feat_emcee(waves[rangemask]), c="crimson", lw=2
+    )
+    axes[1].set_xlabel(r"wavelength ($\mu$m)", fontsize=fs)
+    axes[1].set_ylabel("optical depth", fontsize=fs)
+    axes[1].axhline(ls=":", c="k")
+    plt.savefig(datapath + star + "_62_fit.pdf", bbox_inches="tight")
 
     return fit_result_feat_emcee, chains
 
@@ -384,9 +656,10 @@ def fit_all(datapath, stars, ext_table):
     Saves tables with fitting results
     """
     # list the features to be fitted
-    features = ["58", "10"]
+    features = ["34", "58", "62", "10"]
 
     for feat_name in features:
+        print("Fitting " + feat_name + " feature")
         # create a table to store the results
         result_tab = Table(
             names=(
@@ -516,18 +789,20 @@ def main():
 
     # obtain and plot a stellar model
     stel_mod = Table.read(datapath + "MIRI/tlusty_z100t25000g400v2_miri_ifu.fits")
+    stel_mod = Table.read(datapath + "MIRI/tlusty_z100t23000g400v2_nircam_ss.fits")
     plt.plot(
         stel_mod["WAVELENGTH"] * 1e-4,
         stel_mod["FLUX"] * (stel_mod["WAVELENGTH"] * 1e-4) ** 4,
     )
     # plt.xlim(5.6, 6.1)
-    plt.xlim(7, 14)
+    # plt.xlim(7, 14)
     # plt.ylim(3.2e6, 3.4e6)
     # plt.show()
 
     # obtain the table with extinction properties
     ext_table = Table.read(
-        "/Users/mdecleir/Documents/MEAD/extinction/Gordon+2009_tab2.dat", format="ascii"
+        "/Users/mdecleir/Documents/MEAD/Literature_data/Gordon+2009_tab2.dat",
+        format="ascii",
     )
 
     # fit all features for all stars
