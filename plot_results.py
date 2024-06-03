15d4956e7acb841e0818da98f4e80fc3c9911bc0
mdecleir <mdecleir@stsci.edu>
2024-06-03 14:58:47 -0400

    Added function to plot FM90 parameters

--- plot_results.py
+++ plot_results.py
@@ -275,6 +275,119 @@ def plot_feat_norm_AV_RV(outpath, feat_name, data):
     fig.savefig(outpath + feat_name + "_norm_AV_RV.pdf", bbox_inches="tight")
 
 
+def plot_feat_FM90(outpath, feat_name, data):
+    """
+    Function to plot the (normalized) feature properties vs. the FM90 parameters (UV extinction)
+
+    Parameters
+    ----------
+    outpath : string
+        Path to store the plot
+
+    feat_name : string
+        Reference name for the feature
+
+    data : astropy Table
+        Data to plot
+
+    Returns
+    -------
+    Plots with (normalized) feature properties vs. the FM90 parameters
+    """
+    # define the data and uncertainties to be plotted
+    xpars = [
+        data["C2"],
+        data["C3"] / data["gamma"] ** 2,
+        np.pi * data["C3"] / (2 * data["gamma"]),
+        data["C4"],
+    ]
+    xlabels = [
+        "UV slope = C2",
+        "UV bump amplitude \n = C3/$\gamma^2$",
+        "UV bump area \n = $\pi$C3/(2$\gamma$)",
+        "FUV rise = C4",
+    ]
+
+    C3_unc = np.sqrt(data["C3_runc"] ** 2 + data["C3_sunc"] ** 2)
+    gamma_unc = np.sqrt(data["gamma_runc"] ** 2 + data["gamma_sunc"] ** 2)
+    xuncs = [
+        np.sqrt(data["C2_runc"] ** 2 + data["C2_sunc"] ** 2),
+        data["C3"]
+        / data["gamma"] ** 2
+        * np.sqrt((C3_unc / data["C3"]) ** 2 + (2 * gamma_unc / data["gamma"]) ** 2),
+        np.pi
+        * data["C3"]
+        / (2 * data["gamma"])
+        * np.sqrt((C3_unc / data["C3"]) ** 2 + (gamma_unc / data["gamma"]) ** 2),
+        np.sqrt(data["C4_runc"] ** 2 + data["C4_sunc"] ** 2),
+    ]
+
+    ypars = [
+        "amp/AV",
+        "wavelength(micron)",
+        "std(micron)",
+        "area/AV",
+    ]
+    ylabels = [
+        r"$\tau(\lambda_0)/A(V)$",
+        r"$\lambda_0$ ($\mu$m)",
+        r"$\sigma$ ($\mu$m)",
+        r"$A/A(V)$ (cm$^{-1}$)",
+    ]
+
+    # create the figure
+    fs = 18
+    fig, axes = plt.subplots(
+        len(ypars),
+        len(xpars),
+        figsize=(4 * len(xpars), 4 * len(ypars)),
+        sharex="col",
+        sharey="row",
+    )
+
+    for i, (xpar, xunc, xlabel) in enumerate(zip(xpars, xuncs, xlabels)):
+        # add the x-axis label
+        axes[-1, i].set_xlabel(xlabel, fontsize=fs)
+
+        for j, (ypar, ylabel) in enumerate(zip(ypars, ylabels)):
+            # obtain the y-axis uncertainties
+            if "(" in ypar:
+                index = ypar.find("(")
+                yunc = (
+                    data[ypar[:index] + "_unc_min" + ypar[index:]],
+                    data[ypar[:index] + "_unc_plus" + ypar[index:]],
+                )
+            else:
+                yunc = data[ypar + "_unc_min"], data[ypar + "_unc_plus"]
+
+            # plot the properties
+            axes[j, i].errorbar(
+                xpar,
+                data[ypar],
+                xerr=xunc,
+                yerr=yunc,
+                fmt="ok",
+            )
+
+            # calculate the Spearman correlation coefficient
+            axes[j, i].text(
+                0.05,
+                0.9,
+                r"$\rho = %.2f$" % spearmanr(xpar, data[ypar])[0],
+                transform=axes[j, i].transAxes,
+                fontsize=fs * 0.8,
+                ha="left",
+            )
+
+            # add the y-axis label (once)
+            if i == 0:
+                axes[j, 0].set_ylabel(ylabel, fontsize=fs)
+
+    # finalize and save the figure
+    fig.subplots_adjust(hspace=0, wspace=0)
+    fig.savefig(outpath + feat_name + "_FM90.pdf", bbox_inches="tight")
+
+
 def plot_feat_col(outpath, feat_name, data):
     """
     Function to plot the dust column densities vs. the integrated area of the feature
@@ -323,11 +436,18 @@ def main():
 
     # define the data path and the output path
     datapath = "/Users/mdecleir/Documents/MEAD/Extinction/JWST_data/"
+    litpath = "/Users/mdecleir/Documents/MEAD/Literature_data/"
     outpath = "/Users/mdecleir/Documents/MEAD/Plots/"
 
     # obtain the extinction properties
     ext_table = Table.read(
-        "/Users/mdecleir/Documents/MEAD/Literature_data/Gordon+2009_tab2.dat",
+        litpath + "Gordon+2009_tab2.dat",
+        format="ascii",
+    )
+
+    # obtain the FM90 parameters
+    fm90_table = Table.read(
+        litpath + "Gordon+2009_tab3.dat",
         format="ascii",
     )
 
@@ -343,7 +463,29 @@ def main():
     # merge tables
     joined_ext_58 = join(ext_table, feat_58, keys_left="Name", keys_right="name")
     joined_ext_10 = join(ext_table, feat_10, keys_left="Name", keys_right="name")
+    joined_fm90_10 = join(joined_ext_10, fm90_table, keys="Name")
     joined_dep_10 = join(dep_table, feat_10, keys_left="star", keys_right="name")
+
+    # add extra columns with normalized feature strength and area and uncertainties
+    joined_fm90_10["amp/AV"] = joined_fm90_10["amplitude"] / joined_fm90_10["AV"]
+    joined_fm90_10["area/AV"] = joined_fm90_10["area(cm-1)"] / joined_fm90_10["AV"]
+    AV_unc = np.sqrt(joined_fm90_10["AV_runc"] ** 2 + joined_fm90_10["AV_sunc"] ** 2)
+    joined_fm90_10["amp/AV_unc_min"] = joined_fm90_10["amp/AV"] * np.sqrt(
+        (joined_fm90_10["amplitude_unc_min"] / joined_fm90_10["amplitude"]) ** 2
+        + (AV_unc / joined_fm90_10["AV"]) ** 2
+    )
+    joined_fm90_10["amp/AV_unc_plus"] = joined_fm90_10["amp/AV"] * np.sqrt(
+        (joined_fm90_10["amplitude_unc_plus"] / joined_fm90_10["amplitude"]) ** 2
+        + (AV_unc / joined_fm90_10["AV"]) ** 2
+    )
+    joined_fm90_10["area/AV_unc_min"] = joined_fm90_10["area/AV"] * np.sqrt(
+        (joined_fm90_10["area_unc_min(cm-1)"] / joined_fm90_10["area(cm-1)"]) ** 2
+        + (AV_unc / joined_fm90_10["AV"]) ** 2
+    )
+    joined_fm90_10["area/AV_unc_plus"] = joined_fm90_10["area/AV"] * np.sqrt(
+        (joined_fm90_10["area_unc_plus(cm-1)"] / joined_fm90_10["area(cm-1)"]) ** 2
+        + (AV_unc / joined_fm90_10["AV"]) ** 2
+    )
 
     # define the stars that should be masked
     bad_star = "HD014434"
@@ -356,6 +498,9 @@ def main():
     # plot_feat_AV(outpath, "58", joined_ext_58)
     plot_feat_AV_RV(outpath, "10", joined_ext_10[~bad_mask])
     plot_feat_norm_AV_RV(outpath, "10", joined_ext_10[~bad_mask])
+
+    # create plots vs. the FM90 parameters
+    plot_feat_FM90(outpath, "10", joined_fm90_10[~bad_mask])
 
     # create plot vs. dust column densities
     # plot_feat_col(outpath, "10", joined_dep_10)
